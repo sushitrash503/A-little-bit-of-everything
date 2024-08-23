a0e7415fa249b68b185ef9aad9633f6d22db5ddd
Dcerritos <139426429+sushitrash503@users.noreply.github.com>
2024-08-23 14:38:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -15,4 +15,52 @@ function isMobileDevice() {
   if (isMobileDevice()) {
     showPopup();
   }
+  function makeDraggable(element) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  
+    if (document.querySelector(".window-header")) {
+      document.querySelector(".window-header").onmousedown = dragMouseDown;
+    } else {
+      element.onmousedown = dragMouseDown;
+    }
+  
+    function dragMouseDown(e) {
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      element.style.top = (element.offsetTop - pos2) + "px";
+      element.style.left = (element.offsetLeft - pos1) + "px";
+    }
+  
+    function closeDragElement() {
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  }
+  
+  makeDraggable(document.getElementById("draggable-window"));
+  
+  document.getElementById("toggle-button").addEventListener("click", function() {
+    var content = document.getElementById("window-content");
+    var button = document.getElementById("toggle-button");
+  
+    if (content.style.display === "none") {
+      content.style.display = "block";
+      button.textContent = "Cerrar";
+    } else {
+      content.style.display = "none";
+      button.textContent = "Abrir";
+    }
+    
+  });
   
